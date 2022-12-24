74e17cfb9cb3fc0fdc13265dac97b950780cfaa6
Bella Ananda Putri <bellaanandaputri@student.uns.ac.id>
2022-12-24 20:04:25 +0700

    backend commit

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    /**
+     * index
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function index(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user= User::where('email', $request->email)->first();
+        
+            if (!$user || !Hash::check($request->password, $user->password)) {
+                return response([
+                    'success'   => false,
+                    'message' => ['These credentials do not match our records.']
+                ], 404);
+            }
+        
+            $token = $user->createToken('ApiToken')->plainTextToken;
+        
+            $response = [
+                'success'   => true,
+                'user'      => $user,
+                'token'     => $token
+            ];
+        
+        return response($response, 201);
+    }
+    
+    /**
+     * logout
+     *
+     * @return void
+     */
+    public function logout()
+    {
+        auth()->logout();
+        return response()->json([
+            'success'    => true
+        ], 200);
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'role' => 'required|string'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json(['data' => $user,'access_token' => $token, 'token_type' => 'Bearer', ]);
+    }
+}
